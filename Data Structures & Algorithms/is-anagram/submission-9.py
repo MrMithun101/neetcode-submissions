9c16a4d0dcd66627b42b00e713c2f7fd466a38b4
Mithun Selvananthan <154709796+MrMithun101@users.noreply.github.com>
2026-09-02 17:53:57 -0400

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        s_hashmap = {}
+        t_hashmap = {}
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            s_hashmap[s[i]] = s_hashmap.get(s[i], 0) + 1
+            t_hashmap[t[i]] = t_hashmap.get(t[i], 0) + 1
+
+        if s_hashmap == t_hashmap:
+            return True
+        
+        else:
+            return False
+
+
+
+        
+        
+                 
